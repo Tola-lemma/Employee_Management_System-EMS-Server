f49cf369e12d812d5442f0296e707ddb505fbcbb
Tola-lemma <tolalemma@gmail.com>
2024-12-29 00:57:12 +0300

    feat: route /login  /password-reset, change-password is handled

--- Route/route.js
+++ Route/route.js
@@ -21,7 +21,12 @@ router.get("/employees/:employee_id?", employeeController.getEmployee);
 router.put("/employees/:employee_id", employeeController.updateEmployee);
 router.delete("/employees/:employee_id", employeeController.deleteEmployee);
 
-
-
+//Auth
+const authController = require('../Controller/Auth/Login')
+const passwordReset = require('../Controller/Auth/PasswordReset');
+const changePassword  = require('../Controller/Auth/ChangePassword');
+router.post('/login',authController.loginEmployee)
+router.post('/password-reset/:employee_id',passwordReset.resetPassword)
+router.put('/change-password/:employee_id',changePassword.changePassword)
 
 module.exports = router
