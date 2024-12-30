8419130f54767dbc88e58a4b7376bbcfff133aa0
Natnael Yohannes <163080443+Nat1-Y@users.noreply.github.com>
2024-12-30 16:13:27 +0300

    commit

--- Database/connection.js
+++ Database/connection.js
@@ -1,10 +1,27 @@
-const pg = require('pg');
-require('dotenv').config();
-const db = new pg.Pool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-})
-module.exports = db;
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        logging: false,
+    }
+);
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected successfully!');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+module.exports = { sequelize, connectDB };
