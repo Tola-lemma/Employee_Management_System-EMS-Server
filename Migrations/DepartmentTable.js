9e16bbbe0049567b117cc07c3ac0d9fd99b0949a
Tola-lemma <tolalemma@gmail.com>
2025-01-04 09:56:19 +0300

    BUG: Conflict resolution

--- Migrations/DepartmentTable.js
+++ Migrations/DepartmentTable.js
@@ -1,5 +1,5 @@
 const createDepartmentsTable = `CREATE TABLE IF NOT EXISTS Departments (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(100) NOT NULL UNIQUE
-)`
-module.exports = createDepartmentsTable
+      department_id SERIAL PRIMARY KEY,
+      department_name VARCHAR(100) NOT NULL UNIQUE
+  )`
+  module.exports = createDepartmentsTable
